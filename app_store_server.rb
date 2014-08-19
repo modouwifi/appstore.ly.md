51804e2221e9e20a84d127c01d9da867099159d0
Forrest Ye <afu@forresty.com>
2014-08-19 16:42:00 +0800

    Access-Control-Allow-Origin: *

--- app_store_server.rb
+++ app_store_server.rb
@@ -6,8 +6,10 @@ require File.expand_path("../lib/models/modou/store", __FILE__)
 class AppStoreServer < Sinatra::Application
   configure do
     require 'newrelic_rpm'
+  end
 
-    set :json_content_type, "application/json;charset=utf-8"
+  before do
+    headers 'Access-Control-Allow-Origin' => '*'
   end
 
   get '/apps' do

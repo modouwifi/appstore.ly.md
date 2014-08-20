a8b7252337601293e4cdec27c89671f108bc6d99
Forrest Ye <afu@forresty.com>
2014-08-20 20:17:10 +0800

    sort apps by updated_at, most recently updated first

--- lib/models/modou/store.rb
+++ lib/models/modou/store.rb
@@ -8,7 +8,7 @@ module Modou
           Dir[File.expand_path('../../../../data/*.yml',  __FILE__)].each do |yml_file|
             apps << Modou::Application.create_from_yml(File.read(yml_file))
           end
-        end
+        end.sort_by(&:updated_at).reverse
       end
 
       # return app with app-name

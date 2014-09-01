4020982d036e3689759af0610de1946966cf8302
Forrest Ye <afu@forresty.com>
2014-09-01 11:16:56 +0800

    run simplecov in local

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,8 +1,17 @@
 require "simplecov"
-SimpleCov.start
 
-require "coveralls"
-Coveralls.wear!
+if ENV['SIMPLECOV'] == 'true'
+  SimpleCov.start do
+    add_filter '/spec/'
+  end
+else
+  require "coveralls"
+
+  Coveralls.wear! do
+    add_filter '/spec/'
+  end
+end
+
 
 require File.expand_path("../../lib/models/modou/semantic_version.rb", __FILE__)
 require File.expand_path("../../lib/models/modou/application.rb", __FILE__)

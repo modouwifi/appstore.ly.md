37f7b79d3b806fec9c086334cacb82990fd2509a
Forrest Ye <afu@forresty.com>
2014-09-12 10:57:32 +0800

    use fixtures. fix test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -15,3 +15,13 @@ end
 require File.expand_path("../../lib/models/modou/semantic_version.rb", __FILE__)
 require File.expand_path("../../lib/models/modou/application.rb", __FILE__)
 require File.expand_path("../../lib/models/modou/store.rb", __FILE__)
+
+class Modou::Store
+  def self.all_apps
+    [].tap do |apps|
+      Dir[File.expand_path('../fixtures/data/*.yml',  __FILE__)].each do |yml_file|
+        apps << Modou::Application.create_from_yml(File.read(yml_file))
+      end
+    end
+  end
+end
